020f130c9fca2ec00bc2e2f7405c7f8f4738e3f1
tquang2410 <justicepencil@gmail.com>
2025-10-25 22:44:40 +0700

    First setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    plugins: [
+        require("tailwindcss-animate"),
+    ],
 };
 
 export default nextConfig;
