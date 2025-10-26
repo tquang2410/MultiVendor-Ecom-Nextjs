33ae569ec5036dad59b85f4f55a21c39020b4ed4
tquang2410 <justicepencil@gmail.com>
2025-10-26 10:59:08 +0700

    Customize button component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 
-import { DM_Sans} from "next/dist/compiled/@next/font/dist/google";
+import { DM_Sans} from "next/font/google";
 import "./globals.css";
 
 const dmSans = DM_Sans({
